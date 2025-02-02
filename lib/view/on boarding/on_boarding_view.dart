20793fb8c32260a7ed830d242444781c4dc515bd
1Shibam <hehe.shibam@gmail.com>
2025-02-02 17:48:35 +0530

    update on boarding page

--- lib/view/on boarding/on_boarding_view.dart
+++ lib/view/on boarding/on_boarding_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -8,8 +9,31 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+          child: PageView(
+            controller: pageController,
+            children: const [],
+          ),
+        ),
+        //! Smooth Page Indicator
+        SmoothPageIndicator(
+          controller: pageController,
+          count: 3, // Total pages
+          effect: ExpandingDotsEffect(
+            dotHeight: 8,
+            dotWidth: 8,
+            activeDotColor: Colors.blue, // Change to match your theme
+            dotColor: Colors.grey.shade400,
+          ),
+        ),
+        const SizedBox(height: 40),
+      ],
+    ));
   }
 }
