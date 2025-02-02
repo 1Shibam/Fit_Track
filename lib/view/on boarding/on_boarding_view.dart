96a4f394576c9e2093beb7a317761176cac103f0
1Shibam <hehe.shibam@gmail.com>
2025-02-03 01:18:47 +0530

    update onboarding view!!

--- lib/view/on boarding/on_boarding_view.dart
+++ lib/view/on boarding/on_boarding_view.dart
@@ -23,67 +23,79 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          actions: [
+            Container(
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SKIP',
+                    style: AppTextStyles.body1,
+                  )),
+            )
+          ],
+        ),
         body: Column(
-      children: [
-        Expanded(
-          child: PageView(
-            controller: pageController,
-            children: [
-              const OnBoardingPages(
-                  title: 'Welcome to FitTrack!',
-                  imagePath: 'assets/images/welcome.png'),
-              const OnBoardingPages(
-                  title: 'Track Your Progress',
-                  imagePath: 'assets/images/track.png'),
-              const OnBoardingPages(
-                  title: 'Set Your Fitness Goals',
-                  imagePath: 'assets/images/goals.png'),
-              const OnBoardingPages(
-                  title: 'Track Nutrition',
-                  imagePath: 'assets/images/food.png'),
-              const OnBoardingPages(
-                  title: 'Sync with Health Devices',
-                  imagePath: 'assets/images/devices.png'),
-              const OnBoardingPages(
-                  title: 'Get reminders to keep pushing toward your goals.',
-                  imagePath: 'assets/images/stay motivated.png'),
-              OnBoardingPages(
-                title: 'Start Your Journey!',
-                imagePath: 'assets/images/start.png',
-                button: ElevatedButton(
-                    style: const ButtonStyle(
-                        foregroundColor:
-                            WidgetStatePropertyAll(Colors.transparent),
-                        backgroundColor:
-                            WidgetStatePropertyAll(Colors.transparent),
-                        shadowColor:
-                            WidgetStatePropertyAll(Colors.transparent)),
-                    onPressed: () {},
-                    child: const Text(
-                      'Get Started',
-                      style: AppTextStyles.heading3,
-                    )),
-              )
-            ],
-          ),
-        ),
-        //! Smooth Page Indicator
-        SmoothPageIndicator(
-          onDotClicked: (index) {
-            pageController.jumpToPage(index);
-          },
-          controller: pageController,
-          count: 7, // Total pages
-          effect: WormEffect(
-            radius: 20,
-            dotHeight: 10,
-            dotWidth: 10,
-            activeDotColor: Colors.blue, // Change to match your theme
-            dotColor: AppColors.neutralColorMediumGray,
-          ),
-        ),
-        const SizedBox(height: 40),
-      ],
-    ));
+          children: [
+            Expanded(
+              child: PageView(
+                controller: pageController,
+                children: [
+                  const OnBoardingPages(
+                      title: 'Welcome to FitTrack!',
+                      imagePath: 'assets/images/welcome.png'),
+                  const OnBoardingPages(
+                      title: 'Track Your Progress',
+                      imagePath: 'assets/images/track.png'),
+                  const OnBoardingPages(
+                      title: 'Set Your Fitness Goals',
+                      imagePath: 'assets/images/goals.png'),
+                  const OnBoardingPages(
+                      title: 'Track Nutrition',
+                      imagePath: 'assets/images/food.png'),
+                  const OnBoardingPages(
+                      title: 'Sync with Health Devices',
+                      imagePath: 'assets/images/devices.png'),
+                  const OnBoardingPages(
+                      title: 'Get reminders to keep pushing toward your goals.',
+                      imagePath: 'assets/images/stay motivated.png'),
+                  OnBoardingPages(
+                    title: 'Start Your Journey!',
+                    imagePath: 'assets/images/start.png',
+                    button: ElevatedButton(
+                        style: const ButtonStyle(
+                            foregroundColor:
+                                WidgetStatePropertyAll(Colors.transparent),
+                            backgroundColor:
+                                WidgetStatePropertyAll(Colors.transparent),
+                            shadowColor:
+                                WidgetStatePropertyAll(Colors.transparent)),
+                        onPressed: () {},
+                        child: const Text(
+                          'Get Started',
+                          style: AppTextStyles.heading3,
+                        )),
+                  )
+                ],
+              ),
+            ),
+            //! Smooth Page Indicator
+            SmoothPageIndicator(
+              onDotClicked: (index) {
+                pageController.jumpToPage(index);
+              },
+              controller: pageController,
+              count: 7, // Total pages
+              effect: WormEffect(
+                radius: 20,
+                dotHeight: 10,
+                dotWidth: 10,
+                activeDotColor: Colors.blue, // Change to match your theme
+                dotColor: AppColors.neutralColorMediumGray,
+              ),
+            ),
+            const SizedBox(height: 40),
+          ],
+        ));
   }
 }
