48b1c4da86a04b434f9002c2c191a553a143dad3
1Shibam <hehe.shibam@gmail.com>
2025-02-03 01:18:35 +0530

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Be-Fit',
       theme: ThemeData(
           primaryColor: AppColors.primaryColorRed,
           useMaterial3: true,
           fontFamily: 'Lexend'),
-          
       home: const OnBoardingView(),
     );
   }
