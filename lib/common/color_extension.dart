d0139120cf866c7f32adb16360a52921123b474f
1Shibam <hehe.shibam@gmail.com>
2025-02-02 15:12:29 +0530

    set up app colors!!

---
+++ lib/common/color_extension.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class AppColors {
+
+  //! Primary colors
+  static Color get primaryColorRed => const Color(0xffFF4F00);
+  static Color get primaryColorOrange => const Color(0xffFE7648);
+  static Color get primaryColorBlue => const Color(0xff0171DF);
+
+  //! secondary colors - Accents & Highlights
+  static Color get secondaryColorPurple => const Color(0xffA55FEF);
+  static Color get secondaryColorGreen => const Color(0xff7BC678);
+  static Color get secondaryColorCyan => const Color(0xff00C7D9);
+
+  //! Neutral colors - Backgrounds & Text
+  static Color get neutralColorDarkGray => const Color(0xff1C1C1E);
+  static Color get neutralColorMediumGray => const Color(0xff3A3A3C);
+  static Color get neutralColorSoftWhite => const Color(0xffF4F4F4);
+}
