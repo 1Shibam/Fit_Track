6b253bc8a492d956c8cdde33703790d86b39277d
1Shibam <hehe.shibam@gmail.com>
2025-02-02 18:52:11 +0530

    on boarding view complete, signup and login next!!

--- lib/view/on boarding/on_boarding_view.dart
+++ lib/view/on boarding/on_boarding_view.dart
@@ -1,3 +1,6 @@
+import 'package:be_fit/common/color_extension.dart';
+import 'package:be_fit/common/text_style.dart';
+import 'package:be_fit/view/on%20boarding/on_boarding_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -11,6 +14,13 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   PageController pageController = PageController();
   @override
+  void dispose() {
+    //!implement dispose
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
@@ -18,18 +28,54 @@ class _OnBoardingViewState extends State<OnBoardingView> {
         Expanded(
           child: PageView(
             controller: pageController,
-            children: const [],
+            children: [
+              const OnBoardingPages(
+                  title: 'Welcome to FitTrack!',
+                  imagePath: 'assets/images/welcome.png'),
+              const OnBoardingPages(
+                  title: 'Track Your Progress',
+                  imagePath: 'assets/images/track.png'),
+              const OnBoardingPages(
+                  title: 'Set Your Fitness Goals',
+                  imagePath: 'assets/images/goals.png'),
+              const OnBoardingPages(
+                  title: 'Track Nutrition',
+                  imagePath: 'assets/images/food.png'),
+              const OnBoardingPages(
+                  title: 'Sync with Health Devices',
+                  imagePath: 'assets/images/devices.png'),
+              const OnBoardingPages(
+                  title: 'Get reminders to keep pushing toward your goals.',
+                  imagePath: 'assets/images/stay motivated.png'),
+              OnBoardingPages(
+                title: 'Start Your Journey!',
+                imagePath: 'assets/images/start.png',
+                button: ElevatedButton(
+                    style: const ButtonStyle(
+                        foregroundColor:
+                            WidgetStatePropertyAll(Colors.transparent),
+                        backgroundColor:
+                            WidgetStatePropertyAll(Colors.transparent),
+                        shadowColor:
+                            WidgetStatePropertyAll(Colors.transparent)),
+                    onPressed: () {},
+                    child: const Text(
+                      'Get Started',
+                      style: AppTextStyles.heading3,
+                    )),
+              )
+            ],
           ),
         ),
         //! Smooth Page Indicator
         SmoothPageIndicator(
           controller: pageController,
-          count: 3, // Total pages
+          count: 7, // Total pages
           effect: ExpandingDotsEffect(
             dotHeight: 8,
             dotWidth: 8,
             activeDotColor: Colors.blue, // Change to match your theme
-            dotColor: Colors.grey.shade400,
+            dotColor: AppColors.neutralColorMediumGray,
           ),
         ),
         const SizedBox(height: 40),
