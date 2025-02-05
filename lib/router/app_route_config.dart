6e3c6161b65b1a59125e4feaf3303cd557b4b125
1Shibam <hehe.shibam@gmail.com>
2025-02-05 21:37:06 +0530

    create app route config

---
+++ lib/router/app_route_config.dart
@@ -0,0 +1,39 @@
+import 'package:be_fit/view/auth/login_page.dart';
+import 'package:be_fit/view/auth/signup_page.dart';
+import 'package:be_fit/view/home/home_screen.dart';
+import 'package:be_fit/view/on%20boarding/on_boarding_view.dart';
+import 'package:be_fit/view/splash/splash_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final goRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) {
+      return const SplashScreen();
+    },
+  ),
+  GoRoute(
+    path: '/login',
+    builder: (context, state) {
+      return const LoginPage();
+    },
+  ),
+  GoRoute(
+    path: '/signup',
+    builder: (context, state) {
+      return const SignupPage();
+    },
+  ),
+  GoRoute(
+    path: '/boarding',
+    builder: (context, state) {
+      return const OnBoardingView();
+    },
+  ),
+  GoRoute(
+    path: '/home',
+    builder: (context, state) {
+      return const HomeScreen();
+    },
+  ),
+]);
