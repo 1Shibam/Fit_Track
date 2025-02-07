8cc86e708695414b637b0a6523cae8b7e9868ebd
1Shibam <hehe.shibam@gmail.com>
2025-02-07 23:23:08 +0530

    profile section will finish tomorrow

--- lib/view/complete_profile_view/complete_profile.dart
+++ lib/view/complete_profile_view/complete_profile.dart
@@ -1,4 +1,6 @@
+import 'package:be_fit/common/color_extension.dart';
 import 'package:be_fit/common/text_style.dart';
+import 'package:be_fit/view/complete_profile_view/complete_profile_widgets/tiles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -20,14 +22,55 @@ class _CompleteProfileViewState extends State<CompleteProfileView> {
         padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Lottie.asset('assets/animations/completeProfile.json'),
+            Lottie.asset('assets/animations/completeProfile.json',
+                height: 200.h, width: 200.w),
             SizedBox(
               height: 10.h,
             ),
-            Text(
-              'Let\'s complete your profile',
-              style: AppTextStyles.heading3,
+            Wrap(
+              alignment: WrapAlignment.center,
+              children: [
+                Text(
+                  'Let\'s complete your profile',
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.heading3,
+                ),
+                Text(
+                  'It will help us know more about you!',
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.body2,
+                ),
+                ExpansionTile(
+                  title: Text(
+                    'Gender',
+                    style: AppTextStyles.body1,
+                  ),
+                  backgroundColor:
+                      AppColors.secondaryColorCyan.withOpacity(0.4),
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          Tiles(
+                            tileTitle: 'Male',
+                            trailing: Icon(Icons.male),
+                          ),
+                          Tiles(
+                            tileTitle: 'FeMale',
+                            trailing: Icon(Icons.female),
+                          ),
+                          Tiles(
+                            tileTitle: 'Male',
+                            trailing: Icon(Icons.not_accessible),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
             )
           ],
         ),
