f830cd3337f4b44e696b4de86a4eecd0c895a0f5
1Shibam <hehe.shibam@gmail.com>
2025-02-07 13:49:24 +0530

    used screen util sizes in texts

--- lib/common/text_style.dart
+++ lib/common/text_style.dart
@@ -1,70 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTextStyles {
-
   //! Headings
-  static const TextStyle heading1 = TextStyle(
+  static TextStyle heading1 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 32,
+    fontSize: 32.sp,
     fontWeight: FontWeight.bold,
     color: Colors.black,
   );
 
-  static const TextStyle heading2 = TextStyle(
+  static TextStyle heading2 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 28,
+    fontSize: 28.sp,
     fontWeight: FontWeight.bold,
     color: Colors.black,
   );
 
-  static const TextStyle heading3 = TextStyle(
+  static TextStyle heading3 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 24,
+    fontSize: 24.sp,
     fontWeight: FontWeight.bold,
     color: Colors.black,
   );
 
   //! Subtitles
-  static const TextStyle subtitle1 = TextStyle(
+  static TextStyle subtitle1 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 20,
+    fontSize: 20.sp,
     fontWeight: FontWeight.w600,
     color: Colors.black87,
   );
 
-  static const TextStyle subtitle2 = TextStyle(
+  static TextStyle subtitle2 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 18,
+    fontSize: 18.sp,
     fontWeight: FontWeight.w600,
     color: Colors.black87,
   );
 
   //! Body Text
-  static const TextStyle body1 = TextStyle(
+  static TextStyle body1 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 16,
+    fontSize: 16.sp,
     fontWeight: FontWeight.normal,
     color: Colors.black87,
   );
 
-  static const TextStyle body2 = TextStyle(
+  static TextStyle body2 = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.normal,
     color: Colors.black87,
   );
 
   //! Captions & Small Text
-  static const TextStyle caption = TextStyle(
+  static TextStyle caption = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 12,
+    fontSize: 12.sp,
     fontWeight: FontWeight.w400,
     color: Colors.black54,
   );
 
-  static const TextStyle overline = TextStyle(
+  static TextStyle overline = TextStyle(
     fontFamily: 'Lexend',
-    fontSize: 10,
+    fontSize: 10.sp,
     fontWeight: FontWeight.w400,
     letterSpacing: 1.5,
     color: Colors.black54,
