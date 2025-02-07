79ab0600408cce7fc2b7304428c71deddfeb95bc
1Shibam <hehe.shibam@gmail.com>
2025-02-07 13:50:13 +0530

    fixed lottie animation alignment and size

--- lib/common widgets/lottie_loading_animation.dart
+++ lib/common widgets/lottie_loading_animation.dart
@@ -1,17 +1,41 @@
+import 'package:be_fit/common/color_extension.dart';
+import 'package:be_fit/common/text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class LottieLoadingAnimation extends StatelessWidget {
+  final double opacity;
   const LottieLoadingAnimation({
-    super.key,
+    super.key, this.opacity = 1
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: AppColors.secondaryColorCyan.withOpacity(opacity),
       body: Center(
-        child: Lottie.asset('assets/animations/fitness lottie json.json'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/animations/fitness lottie json.json',
+                height: 300.r, width: 300.r),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Loading',
+                  style: AppTextStyles.body1,
+                ),
+                AnimatedTextKit(animatedTexts: [
+                  TyperAnimatedText('....',
+                      speed: const Duration(milliseconds: 500))
+                ])
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
