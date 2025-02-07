a9198a67da3b51f1ae8bf75080ceaadc1ae1312b
1Shibam <hehe.shibam@gmail.com>
2025-02-07 15:26:12 +0530

    complete the login page auth is still to be done

--- lib/view/auth/login_page.dart
+++ lib/view/auth/login_page.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:be_fit/common%20widgets/build_primary_button.dart';
 import 'package:be_fit/common%20widgets/build_text_field.dart';
-import 'package:be_fit/common%20widgets/lottie_loading_animation.dart';
 import 'package:be_fit/common/color_extension.dart';
 import 'package:be_fit/common/text_style.dart';
 
@@ -11,57 +8,79 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Welcome Back!", style: AppTextStyles.heading1),
-            SizedBox(height: 8.h),
-            Text("Login to continue", style: AppTextStyles.subtitle1),
-            SizedBox(height: 32.h),
-            const BuildTextField(
-              label: 'Email',
-            ),
-            SizedBox(height: 16.h),
-            const BuildTextField(
-              label: 'Password',
-            ),
-            SizedBox(height: 24.h),
-            const BuildPrimaryButton(text: 'Login'),
-            SizedBox(height: 16.h),
-            Center(
-              child: RichText(
-                  text: TextSpan(children: [
-                TextSpan(
-                    text: 'Don\'t have an account?',
-                    style: AppTextStyles.body2),
-                TextSpan(
-                    text: ' Sign Up',
-                    style: AppTextStyles.body2
-                        .copyWith(color: AppColors.primaryColorRed),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        const LottieLoadingAnimation(
-                          opacity: 0.5,
-                        );
-                        Future.delayed(
-                          const Duration(seconds: 1),
-                          () {
-                            context.go('/signup');
-                          },
-                        );
-                      })
-              ])),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Login to continue",
+                style: AppTextStyles.heading1,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 32.h),
+              BuildTextField(
+                preWidget: Icon(
+                  Icons.email,
+                  color: AppColors.primaryColorOrange,
+                  size: 24.h,
+                ),
+                label: 'Email',
+              ),
+              SizedBox(height: 16.h),
+              BuildTextField(
+                label: 'Password',
+                isPassword: isVisible,
+                preWidget: Icon(
+                  Icons.lock,
+                  color: AppColors.primaryColorOrange,
+                  size: 24.h,
+                ),
+                suffWidget: IconButton(
+                    onPressed: () => setState(() {
+                          isVisible = !isVisible;
+                        }),
+                    icon: Icon(
+                        isVisible ? Icons.visibility_off : Icons.visibility),
+                    color: isVisible
+                        ? AppColors.neutralColorMediumGray
+                        : AppColors.primaryColorOrange),
+              ),
+              SizedBox(height: 24.h),
+              const BuildPrimaryButton(text: 'Login'),
+              SizedBox(height: 16.h),
+              Center(
+                child: RichText(
+                    text: TextSpan(children: [
+                  TextSpan(
+                      text: 'Don\'t have an account?',
+                      style: AppTextStyles.body2),
+                  TextSpan(
+                      text: ' Sign Up',
+                      style: AppTextStyles.body2
+                          .copyWith(color: AppColors.primaryColorRed),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          context.go('/signup');
+                        })
+                ])),
+              ),
+            ],
+          ),
         ),
       ),
     );
