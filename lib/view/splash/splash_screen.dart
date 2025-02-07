047719e514ab803d96fd84dc71374601c9223e90
1Shibam <hehe.shibam@gmail.com>
2025-02-07 22:35:52 +0530

    update the splash screen for new preference

--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -13,11 +13,8 @@ class SplashScreen extends StatelessWidget {
     final prefs = await SharedPreferences.getInstance();
     bool hasSeenOnboarding = prefs.getBool('hasSeenOnBoarding') ?? false;
     bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
-    return [hasSeenOnboarding, isLoggedIn];
-  }
-
-  Widget _buildLottieAnimation() {
-    return const LottieLoadingAnimation();
+    bool hasSetProfile = prefs.getBool('hasSetProfile') ?? false;
+    return [hasSeenOnboarding, isLoggedIn, hasSetProfile];
   }
 
   @override
@@ -26,7 +23,7 @@ class SplashScreen extends StatelessWidget {
       future: getPreferences(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return _buildLottieAnimation();
+          return const LottieLoadingAnimation();
         }
 
         if (!snapshot.hasData || snapshot.data == null) {
@@ -35,19 +32,22 @@ class SplashScreen extends StatelessWidget {
 
         bool hasSeenOnboarding = snapshot.data![0];
         bool isLoggedIn = snapshot.data![1];
+        bool hasSetProfile = snapshot.data![2];
 
         // Delay before navigation to show the animation
-        Future.delayed(const Duration(seconds: 2), () {
+        Future.delayed(const Duration(seconds: 1), () {
           if (!hasSeenOnboarding) {
             context.go('/boarding');
           } else if (!isLoggedIn) {
             context.go('/login');
+          } else if (!hasSetProfile) {
+            context.go('/completeProfile');
           } else {
             context.go('/home');
           }
         });
 
-        return _buildLottieAnimation(); // Show animation while processing
+        return const LottieLoadingAnimation(); // Show animation while processing
       },
     );
   }
