28bee231a5eb774f290a17f1e489c0953eedff9e
1Shibam <hehe.shibam@gmail.com>
2025-02-07 15:25:40 +0530

    update theme data

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -5,16 +5,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 final ThemeData appTheme = ThemeData(
   inputDecorationTheme: InputDecorationTheme(
+    prefixIconColor: AppColors.primaryColorOrange,
+    suffixIconColor: AppColors.primaryColorOrange,
+    floatingLabelStyle: TextStyle(color: AppColors.primaryColorOrange),
     filled: true,
     fillColor: Colors.white,
     border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.r)),
     enabledBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(16.r),
+      borderRadius: BorderRadius.circular(12.r),
       borderSide: BorderSide(color: AppColors.neutralColorMediumGray),
     ),
     focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(16.r),
-      borderSide: BorderSide(color: AppColors.primaryColorBlue, width: 2),
+      borderRadius: BorderRadius.circular(20.r),
+      borderSide: BorderSide(color: AppColors.primaryColorOrange, width: 2),
+    ),
+    activeIndicatorBorder: BorderSide(
+      color: AppColors.primaryColorOrange, width: 1
     ),
     errorBorder: OutlineInputBorder(
       borderRadius: BorderRadius.circular(16.r),
