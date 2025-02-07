b0cf17230bb936934c2e37839db7a0ea2ce838d7
1Shibam <hehe.shibam@gmail.com>
2025-02-07 13:49:46 +0530

    build a primary button

---
+++ lib/common widgets/build_primary_button.dart
@@ -0,0 +1,25 @@
+import 'package:be_fit/common/color_extension.dart';
+import 'package:be_fit/common/text_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BuildPrimaryButton extends StatelessWidget {
+  final String text;
+  const BuildPrimaryButton({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 12.h),
+      decoration: BoxDecoration(
+        gradient: AppColors.primaryGradient,
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+      child: Center(
+        child: Text(text,
+            style: AppTextStyles.heading3.copyWith(color: Colors.white)),
+      ),
+    );
+  }
+}
