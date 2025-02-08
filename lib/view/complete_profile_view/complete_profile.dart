ecafedfeabd5fc562098093ccc97e8f531d09207
1Shibam <hehe.shibam@gmail.com>
2025-02-08 11:36:20 +0530

    profile creation page done!

--- lib/view/complete_profile_view/complete_profile.dart
+++ lib/view/complete_profile_view/complete_profile.dart
@@ -1,80 +1,151 @@
 import 'package:be_fit/common/color_extension.dart';
 import 'package:be_fit/common/text_style.dart';
-import 'package:be_fit/view/complete_profile_view/complete_profile_widgets/tiles.dart';
+import 'package:be_fit/view/auth/auth_widgets/build_primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:lottie/lottie.dart';
 
 class CompleteProfileView extends StatefulWidget {
   const CompleteProfileView({super.key});
 
   @override
-  State<CompleteProfileView> createState() => _CompleteProfileViewState();
+  CompleteProfileViewState createState() => CompleteProfileViewState();
 }
 
-class _CompleteProfileViewState extends State<CompleteProfileView> {
+class CompleteProfileViewState extends State<CompleteProfileView> {
+  String? selectedGender;
+  DateTime? selectedDate;
+  String weightUnit = "kg";
+  String heightUnit = "cm";
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  void _selectDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: AppColors.neutralColorSoftWhite,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Lottie.asset('assets/animations/completeProfile.json',
-                height: 200.h, width: 200.w),
-            SizedBox(
-              height: 10.h,
-            ),
-            Wrap(
-              alignment: WrapAlignment.center,
-              children: [
-                Text(
-                  'Let\'s complete your profile',
-                  textAlign: TextAlign.center,
-                  style: AppTextStyles.heading3,
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100.h,
+              ),
+              //! simple animation
+              Lottie.asset(
+                'assets/animations/completeProfile.json',
+                height: 240.h,
+                width: 240.w,
+              ),
+              //! headers
+              Text(
+                'Let\'s complete your profile',
+                style: AppTextStyles.heading3,
+                textAlign: TextAlign.center,
+              ),
+
+              //! Gender Selection
+              _buildCard(
+                title: "Gender",
+                child: DropdownButtonFormField<String>(
+                  decoration:
+                      const InputDecoration(border: OutlineInputBorder()),
+                  value: selectedGender,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedGender = newValue;
+                    });
+                  },
+                  items: ["Male", "Female", "Other"]
+                      .map((gender) => DropdownMenuItem(
+                            value: gender,
+                            child: Text(gender),
+                          ))
+                      .toList(),
                 ),
-                Text(
-                  'It will help us know more about you!',
-                  textAlign: TextAlign.center,
-                  style: AppTextStyles.body2,
+              ),
+
+              //! Date of Birth Picker
+              _buildCard(
+                title: "Date of Birth",
+                child: ListTile(
+                  title: Text(selectedDate == null
+                      ? "Select Date"
+                      : "${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}"),
+                  trailing: const Icon(Icons.calendar_today),
+                  onTap: () => _selectDate(context),
                 ),
-                ExpansionTile(
-                  title: Text(
-                    'Gender',
-                    style: AppTextStyles.body1,
+              ),
+
+              //! Weight Input (kg)
+              _buildCard(
+                title: "Weight",
+                child: TextField(
+                  controller: weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter weight (kg)",
                   ),
-                  backgroundColor:
-                      AppColors.secondaryColorCyan.withOpacity(0.4),
-                  children: [
-                    Expanded(
-                      child: ListView(
-                        children: [
-                          Tiles(
-                            tileTitle: 'Male',
-                            trailing: Icon(Icons.male),
-                          ),
-                          Tiles(
-                            tileTitle: 'FeMale',
-                            trailing: Icon(Icons.female),
-                          ),
-                          Tiles(
-                            tileTitle: 'Male',
-                            trailing: Icon(Icons.not_accessible),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                )
-              ],
-            )
+                ),
+              ),
+
+              //! Height Input (cm)
+              _buildCard(
+                title: "Height",
+                child: TextField(
+                  controller: heightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter height (cm)",
+                  ),
+                ),
+              ),
+              GestureDetector(
+                  onTap: () {}, child: const BuildPrimaryButton(text: 'Next'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard({required String title, required Widget child}) {
+    return Card(
+      color: Colors.white,
+      margin: const EdgeInsets.only(bottom: 12),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title,
+                style: AppTextStyles.subtitle1), // Changed to subtitle1 style
+            const SizedBox(height: 8),
+            child,
           ],
         ),
       ),
-    ));
+    );
   }
 }
