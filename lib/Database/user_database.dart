dfd45c0c24ef9d877272997f1eb610540208af50
1Shibam <hehe.shibam@gmail.com>
2025-02-08 14:07:24 +0530

    data base provider done

--- lib/Database/user_database.dart
+++ lib/Database/user_database.dart
@@ -1,5 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
-final userDatabaeProvider = FutureProvider((ref) async {
-  
+final userDatabaseProvider = FutureProvider((ref) async {
+  final dbPath = await getApplicationDocumentsDirectory();
+  String path = join(dbPath.path, 'fit.db');
+
+  Database database = await openDatabase(
+    path,
+    version: 1,
+    onCreate: (db, version) {
+      db.execute('''
+CREATE TABLE fitTrack( userName TEXT, userEmal TEXT, userAge REAL, userWeight REAL, userHeight REAL)
+
+
+''');
+    },
+  );
 });
