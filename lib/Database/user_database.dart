2c186d3916c6489c4c1efc24fbf1fe18e9f13403
1Shibam <hehe.shibam@gmail.com>
2025-02-08 14:28:55 +0530

    update the database

--- lib/Database/user_database.dart
+++ lib/Database/user_database.dart
@@ -3,7 +3,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-final userDatabaseProvider = FutureProvider((ref) async {
+final userDatabaseProvider = FutureProvider<Database>((ref) async {
   final dbPath = await getApplicationDocumentsDirectory();
   String path = join(dbPath.path, 'fit.db');
 
@@ -12,10 +12,11 @@ final userDatabaseProvider = FutureProvider((ref) async {
     version: 1,
     onCreate: (db, version) {
       db.execute('''
-CREATE TABLE fitTrack( userName TEXT, userEmal TEXT, userAge REAL, userWeight REAL, userHeight REAL)
+CREATE TABLE fitTrack(  userId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, userEmail TEXT, userAge REAL, userWeight REAL, userHeight REAL)
 
 
 ''');
     },
   );
+  return database;
 });
