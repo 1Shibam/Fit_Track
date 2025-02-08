e5ac940d302270954eb9d3a141ad0f34a5092db0
1Shibam <hehe.shibam@gmail.com>
2025-02-08 12:35:28 +0530

    minor change after firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
-
 import 'package:be_fit/common/theme.dart';
+import 'package:be_fit/firebase_options.dart';
 import 'package:be_fit/router/app_route_config.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,16 +24,11 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       builder: (context, child) {
         return MaterialApp.router(
-          routerConfig: goRouter,
-
-          debugShowCheckedModeBanner: false,
-          title: 'Be-Fit',
-          theme: appTheme
-          
-        );
+            routerConfig: goRouter,
+            debugShowCheckedModeBanner: false,
+            title: 'Be-Fit',
+            theme: appTheme);
       },
     );
   }
-
-
 }
