9cf79ab7460c07a554002084453da60cb6303f77
1Shibam <hehe.shibam@gmail.com>
2025-02-09 18:17:32 +0530

    create firebase instance

--- lib/services/fireabase_auth_methods.dart
+++ lib/services/fireabase_auth_methods.dart
@@ -1,1 +1,6 @@
-class FireabaseAuthMethods {}
+import 'package:firebase_core/firebase_core.dart';
+
+class FireabaseAuthMethods {
+  final Firebase firebase; //firebase instance
+  FireabaseAuthMethods(this.firebase);
+}
