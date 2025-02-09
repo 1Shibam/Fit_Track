11e709c7a300ce5d6dedb46c0b689f1f4ad22a44
1Shibam <hehe.shibam@gmail.com>
2025-02-09 19:29:13 +0530

    email verification method

--- lib/services/fireabase_auth_methods.dart
+++ lib/services/fireabase_auth_methods.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
 
+import 'package:be_fit/common%20widgets/build_snackbar.dart';
+import 'package:be_fit/common/color_extension.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/widgets.dart';
@@ -17,9 +20,20 @@ class FireabaseAuthMethods {
     try {
       await fAuth.createUserWithEmailAndPassword(
           email: email, password: password);
+      await emailVerification(context);
     } on FirebaseAuthException catch (e) {
       print(e.code == 'Weak-password');
+    }
+  }
 
+  //! Email verification can't let fake emails sign up can we?
+
+  Future<void> emailVerification(BuildContext context) async {
+    try {
+      await fAuth.currentUser!.sendEmailVerification();
+    } on FirebaseException catch (e) {
+      buildSnackBar(context, e.message.toString(),
+          bgColor: AppColors.primaryColorRed);
     }
   }
 }
