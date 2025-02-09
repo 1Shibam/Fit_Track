ed2653a5d0a5f4d699873ecb7762c9aeee854af7
1Shibam <hehe.shibam@gmail.com>
2025-02-09 19:06:51 +0530

    email signup mehtod complete

--- lib/services/fireabase_auth_methods.dart
+++ lib/services/fireabase_auth_methods.dart
@@ -1,6 +1,25 @@
-import 'package:firebase_core/firebase_core.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/widgets.dart';
 
 class FireabaseAuthMethods {
-  final Firebase firebase; //firebase instance
-  FireabaseAuthMethods(this.firebase);
+  final FirebaseAuth fAuth; //firebase instance
+  FireabaseAuthMethods(this.fAuth);
+
+  //! Signup with email
+  Future<void> signUpWithEmail(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    //? using context here to pass some widgets like snackbar and alret dialog for proper auth messages
+
+    try {
+      await fAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      print(e.code == 'Weak-password');
+
+    }
+  }
 }
