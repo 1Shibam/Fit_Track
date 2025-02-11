153e93b98f92a52b364210fd360643a12669d34d
1Shibam <hehe.shibam@gmail.com>
2025-02-11 15:10:03 +0530

    working with otp dialog widge

--- lib/presentation/screens/auth/auth_widgets/show_otp_dialog.dart
+++ lib/presentation/screens/auth/auth_widgets/show_otp_dialog.dart
@@ -1,19 +1,27 @@
+import 'package:be_fit/core/constants/color_extension.dart';
 import 'package:be_fit/core/constants/text_style.dart';
+import 'package:be_fit/presentation/providers/router/app_route_config.dart';
+import 'package:be_fit/presentation/screens/auth/auth_widgets/build_text_field.dart';
+import 'package:be_fit/presentation/widgets/common%20widgets/build_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 final countDownProvider = StateProvider<int>((ref) => 30);
+
 void showOtpDialog({
   required String codeController,
   required BuildContext context,
   required VoidCallback onPressed,
+  required WidgetRef ref,
 }) {
   showDialog(
+    barrierDismissible: false,
     context: context,
     builder: (context) {
       return AlertDialog(
-        contentPadding: EdgeInsets.all(16.w),
+        contentPadding: EdgeInsets.all(12.w),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -21,30 +29,67 @@ void showOtpDialog({
           children: [
             Text(
               'Enter OTP',
-              style: AppTextStyles.subtitle1,
+              style: AppTextStyles.heading3,
             ),
             SizedBox(
               height: 12.h,
             ),
-            Text(
-              'Enter the OTP sent to you mobile number ${codeController}',
-              style: AppTextStyles.body2,
+            RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                    text: 'Enter the OTP sent to ',
+                    style: AppTextStyles.subtitle2,
+                    children: [
+                      TextSpan(
+                          text: codeController.substring(0, 5).padRight(
+                                codeController.length,
+                                'X',
+                              ),
+                          style: AppTextStyles.body1.copyWith(
+                              color: const Color.fromARGB(255, 0, 120, 6)))
+                    ])),
+            SizedBox(
+              height: 12.h,
+            ),
+            const BuildTextField(
+              label: 'Enter OTP',
+              preWidget: Icon(Icons.verified),
             )
           ],
         ),
         actions: [
-          Stack(
-            children: [
-              TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Resend',
-                    style: AppTextStyles.body2,
-                  )),
-            ],
+          TextButton(
+              onPressed: () {
+                context.go('/signup');
+                buildSnackBar(context, 'Something went wrong!',
+                    bgColor: AppColors.primaryColorRed);
+              },
+              child: Text(
+                'Cancel',
+                style: AppTextStyles.body2,
+              )),
+          Consumer(
+            builder: (context, ref, child) {
+              final countDown = ref.watch(countDownProvider);
+              return countDown > 0
+                  ? Text('Resend in $countDown s')
+                  : TextButton(
+                      onPressed: () {
+                        // Reset countdown and restart it
+                        ref.read(countDownProvider.notifier).state = 30;
+                        startCountdown(ref);
+                      },
+                      child: Text(
+                        'Resend',
+                        style: AppTextStyles.body2,
+                      ));
+            },
           ),
           TextButton(
-              onPressed: () {},
+              onPressed: () {
+                ref.read(countDownProvider.notifier).state = 30;
+                startCountdown(ref);
+              },
               child: Text(
                 'Verify',
                 style: AppTextStyles.body2,
@@ -53,6 +98,17 @@ void showOtpDialog({
       );
     },
   );
+  startCountdown(ref); // Start countdown immediately when the dialog is shown.
 }
 
-// 32:23 continue tomorrow right here....
+void startCountdown(WidgetRef ref) {
+  // Use Future.delayed to update the countdown every second
+  Future.delayed(Duration.zero, () {
+    // Every second, decrement the countdown until it reaches 0
+    for (int i = 30; i >= 0; i--) {
+      Future.delayed(Duration(seconds: 30 - i), () {
+        ref.read(countDownProvider.notifier).state = i;
+      });
+    }
+  });
+}
