b52e720232143be5a34bccd48d9d06f243757813
1Shibam <hehe.shibam@gmail.com>
2025-02-11 11:16:24 +0530

    updated the otp dialog

--- lib/presentation/screens/auth/auth_widgets/show_otp_dialog.dart
+++ lib/presentation/screens/auth/auth_widgets/show_otp_dialog.dart
@@ -1,7 +1,11 @@
+import 'package:be_fit/core/constants/text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+final countDownProvider = StateProvider<int>((ref) => 30);
 void showOtpDialog({
-  required TextEditingController codeController,
+  required String codeController,
   required BuildContext context,
   required VoidCallback onPressed,
 }) {
@@ -9,7 +13,43 @@ void showOtpDialog({
     context: context,
     builder: (context) {
       return AlertDialog(
-        actions: [],
+        contentPadding: EdgeInsets.all(16.w),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Enter OTP',
+              style: AppTextStyles.subtitle1,
+            ),
+            SizedBox(
+              height: 12.h,
+            ),
+            Text(
+              'Enter the OTP sent to you mobile number ${codeController}',
+              style: AppTextStyles.body2,
+            )
+          ],
+        ),
+        actions: [
+          Stack(
+            children: [
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Resend',
+                    style: AppTextStyles.body2,
+                  )),
+            ],
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                'Verify',
+                style: AppTextStyles.body2,
+              )),
+        ],
       );
     },
   );
